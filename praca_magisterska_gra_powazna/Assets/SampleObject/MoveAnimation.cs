c141877840170267fb77deb6783d731b4ceaf356
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-03-26 09:11:28 +0200

    Centralization of brain lobe

--- praca_magisterska_gra_powazna/Assets/SampleObject/MoveAnimation.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/MoveAnimation.cs
@@ -7,19 +7,19 @@ public class MoveAnimation : MonoBehaviour
     public bool isSideLeftAnimationOn = false;
     public Transform brainPositionForDisplayingText;
     public Transform cameraCentralPoint;
+    public Transform brainContainer;
     private float movementSpeed = 6.5f;
-
+    private Vector3 vectorCentreOfLobeToCentreOfBrain;
+    public bool imChoosenLobe = false;
 
     public BrainDescriptionText brainDescriptionText;
+
 
     // Update is called once per frame
     void Update()
     {
-        //TODO
-        // * change scale during movement to left
-        // * add click & press diff
-        // * add scaling part of brain during movement to left & enable color
-
+        vectorCentreOfLobeToCentreOfBrain = brainContainer.transform.position - transform.position;
+        //UnityEngine.Debug.Log("xd = " + vectorCentreOfLobeToCentreOfBrain);
         setTextVisibility(false);
 
         if (isSideLeftAnimationOn)
@@ -28,7 +28,11 @@ public class MoveAnimation : MonoBehaviour
         }
         else
         {
-            brainCentralizationMovement();
+            if (imChoosenLobe)
+            {
+                brainCentralizationMovement();
+            }
+            
         }
     }
 
@@ -46,9 +50,23 @@ public class MoveAnimation : MonoBehaviour
 
     void moveBrainLeft()
     {
-        transform.position = Vector3.Lerp(transform.position, brainPositionForDisplayingText.transform.position, Time.deltaTime * movementSpeed);
-        
-        if(brainPositionForDisplayingText.transform.position.x - transform.position.x < 0.3f)
+        //transform.position = Vector3.Lerp(transform.position, brainPositionForDisplayingText.transform.position, Time.deltaTime * movementSpeed);
+
+        //if(brainPositionForDisplayingText.transform.position.x - transform.position.x < 0.3f)
+        //{
+        //    setTextVisibility(true);
+        //}
+        //else
+        //{
+        //    setTextVisibility(false);
+        //}
+
+        brainContainer.transform.position = Vector3.Lerp(
+                brainContainer.transform.position, 
+                brainPositionForDisplayingText.transform.position + vectorCentreOfLobeToCentreOfBrain, 
+                Time.deltaTime * movementSpeed);
+
+        if (brainPositionForDisplayingText.transform.position.x - brainContainer.transform.position.x < 0.3f)
         {
             setTextVisibility(true);
         }
@@ -60,7 +78,13 @@ public class MoveAnimation : MonoBehaviour
 
     void brainCentralizationMovement()
     {
-        transform.position = Vector3.Lerp(transform.position, cameraCentralPoint.transform.position, Time.deltaTime * movementSpeed);
+        //transform.position = Vector3.Lerp(transform.position, cameraCentralPoint.transform.position, Time.deltaTime * movementSpeed);
+        //setTextVisibility(false);
+        brainContainer.transform.position = Vector3.Lerp(
+            brainContainer.transform.position,
+            cameraCentralPoint.transform.position,
+            Time.deltaTime * movementSpeed);
+
         setTextVisibility(false);
     }
 }
