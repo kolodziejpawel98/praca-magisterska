44059b80aa54b12acf7d7f6c441d3b103157f87c
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-04-20 10:49:01 +0200

    kolejnosc ekranow

--- praca_magisterska_gra_powazna/Assets/MainMenuElements/MainMenu.cs
+++ praca_magisterska_gra_powazna/Assets/MainMenuElements/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void nextScreen()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
