c8399a4c599b814cd4f647d952357d0ec29aa317
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-04-20 11:09:52 +0200

    Answer class

--- praca_magisterska_gra_powazna/Assets/SampleObject/TestBackend.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/TestBackend.cs
@@ -3,39 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//enum answer
-//{
-//    A,
-//    B,
-//    C,
-//    D
-//}
+public class Answer
+{
+    public Button answerButton;
+    public string answerText;
+    public bool isAnswerCorrect;
+    public bool isButtonActive;
 
-
-//public class Question
-//{
-//    string questionText;
-//    Dictionary<string, bool> answer = new Dictionary<string, bool>();
-
-//    public Question(string questionText,
-//                    string answer_A, bool isAnswerAcorrect,
-//                    string answer_B, bool isAnswerBcorrect,
-//                    string answer_C, bool isAnswerCcorrect,
-//                    string answer_D, bool isAnswerDcorrect
-//                    )
-//    {
-//        this.questionText = questionText;
-//        answer.Add(answer_A, isAnswerAcorrect);
-//        answer.Add(answer_B, isAnswerBcorrect);
-//        answer.Add(answer_C, isAnswerCcorrect);
-//        answer.Add(answer_D, isAnswerDcorrect);
-//    }
-
-//    public void print()
-//    {
-//        p.r(answer.Count.ToString());
-//    }
-//}
+    public Answer(Button answerButton, string answerText, bool isAnswerCorrect, bool isButtonActive = true)
+    {
+        this.answerText = answerText;
+        this.answerButton = answerButton;
+        this.isAnswerCorrect = isAnswerCorrect;
+        this.isButtonActive = isButtonActive;
+    }
+}
 
 public class TestBackend : MonoBehaviour
 {
@@ -43,49 +25,20 @@ public class TestBackend : MonoBehaviour
     public Text scoreText;
     private int score = 0;
     public int currentQuestion = 1;
-    //List<Question> questions = new List<Question> ();
-
-
-    //string question_1;
-    //Dictionary<string, bool> question_1_answers = new Dictionary<string, bool>(){
-    //    { "Odpowiedz A", false },
-    //    { "Odpowiedz B", false },
-    //    { "Odpowiedz C", false }
-    //};
-    //string question_2;
-    //Dictionary<string, bool> question_2_answers = new Dictionary<string, bool>(){
-    //    { "Klucz1", false },
-    //    { "Klucz2", false },
-    //    { "Klucz3", false }
-    //};
-    //string question_3;
-    //Dictionary<string, bool> question_3_answers = new Dictionary<string, bool>(){
-    //    { "Klucz1", false },
-    //    { "Klucz2", false },
-    //    { "Klucz3", false }
-    //};
-    //string question_4;
-    //Dictionary<string, bool> question_4_answers = new Dictionary<string, bool>(){
-    //    { "Klucz1", false },
-    //    { "Klucz2", false },
-    //    { "Klucz3", false }
-    //};
-
 
     public Text question;
-
-    public Button answer_A;
-    public Button answer_B;
-    public Button answer_C;
-    public Button answer_D;
+    public Button answer_A_button;
+    public Button answer_B_button;
+    public Button answer_C_button;
+    public Button answer_D_button;
 
     void Start()
     {
         printTextElementOnScreen(question);
-        printTextElementOnScreen(answer_A);
-        printTextElementOnScreen(answer_B);
-        printTextElementOnScreen(answer_C);
-        printTextElementOnScreen(answer_D);
+        printTextElementOnScreen(answer_A_button);
+        printTextElementOnScreen(answer_B_button);
+        printTextElementOnScreen(answer_C_button);
+        printTextElementOnScreen(answer_D_button);
     }
 
     private void Update()
@@ -95,30 +48,29 @@ public class TestBackend : MonoBehaviour
         {
             case 1:
                 updateTextElementOnScreen(question, "Który p³at mózgowy nie istnieje?");
-                updateTextElementOnScreen(answer_A, "czo³owy");
-                updateTextElementOnScreen(answer_B, "polityczny");
-                updateTextElementOnScreen(answer_C, "skroniowy");
-                updateTextElementOnScreen(answer_D, "ciemieniowy");
+                updateTextElementOnScreen(new Answer(answer_A_button, "czo³owy", false));
+                updateTextElementOnScreen(new Answer(answer_B_button, "polityczny", false));
+                updateTextElementOnScreen(new Answer(answer_C_button, "skroniowy", false));
+                updateTextElementOnScreen(new Answer(answer_D_button, "ciemieniowy", false));
                 break;
 
             case 2:
                 updateTextElementOnScreen(question, "Lewa pó³kula odpowiada za:");
-                updateTextElementOnScreen(answer_A, "logikê");
-                updateTextElementOnScreen(answer_B, "kreatywnoœæ");
-                updateTextElementOnScreen(answer_C, "-", false);
-                updateTextElementOnScreen(answer_D, "-", false);
+                updateTextElementOnScreen(new Answer(answer_A_button, "logikê", false));
+                updateTextElementOnScreen(new Answer(answer_B_button, "kreatywnoœæ", false));
+                updateTextElementOnScreen(new Answer(answer_C_button, "-", false, false));
+                updateTextElementOnScreen(new Answer(answer_D_button, "-", false, false));
                 break;
             case 3:
                 updateTextElementOnScreen(question, "Osoba uderzy³a siê mocno w ty³ g³owy, i uszkodzi³a (?) p³at potyliczny. Jaki zmys³ móg³ ucierpieæ?", 20);
-                updateTextElementOnScreen(answer_A, "wzrok");
-                updateTextElementOnScreen(answer_B, "s³uch");
-                updateTextElementOnScreen(answer_C, "mowa");
-                updateTextElementOnScreen(answer_D, "wêch");
+                updateTextElementOnScreen(new Answer(answer_A_button, "wzrok", false));
+                updateTextElementOnScreen(new Answer(answer_B_button, "s³uch", false));
+                updateTextElementOnScreen(new Answer(answer_C_button, "mowa", false));
+                updateTextElementOnScreen(new Answer(answer_D_button, "wêch", false));
                 break;
 
         }
     }
-
 
     public void printTextElementOnScreen(Button answer, string text = "", int x = -426, int y = 39, int fontSize = 47)
     {
@@ -139,6 +91,20 @@ public class TestBackend : MonoBehaviour
         newText.fontSize = fontSize;
         RectTransform rectTransform = newText.GetComponent<RectTransform>();
         rectTransform.anchoredPosition = new Vector2(x, y);
+    }
+
+    public void updateTextElementOnScreen(Answer answer)
+    {
+        if (answer.isButtonActive)
+        {
+            answer.answerButton.interactable = true;
+        }
+        else
+        {
+            answer.answerButton.interactable = false;
+        }
+
+        answer.answerButton.GetComponentInChildren<Text>().text = answer.answerText;
     }
 
     public void updateTextElementOnScreen(Button answer, string text, bool isButtonActive = true)
