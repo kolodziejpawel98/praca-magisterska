aa09d7201747d42e1b982be216c856f523ad3351
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-05-29 12:03:57 +0200

    wip

---
+++ praca_magisterska_gra_powazna/Assets/SampleObject/SpriteBackend.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteBackend : MonoBehaviour
+{
+    public static bool isSpriteInClickableMode = true;
+
+}
--- praca_magisterska_gra_powazna/Assets/SampleObject/SpriteHover.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/SpriteHover.cs
@@ -27,70 +27,86 @@ public class SpriteHover : MonoBehaviour
 
     private void OnMouseEnter()
     {
-        spriteRenderer.color = new Color(5.0f, 0.0f, 0.0f, 0.9f);
+        if (SpriteBackend.isSpriteInClickableMode)
+        {
+            spriteRenderer.color = new Color(5.0f, 0.0f, 0.0f, 0.9f);
+        }
     }
 
     private void OnMouseExit()
     {
-        spriteRenderer.color = originalSpriteColor;
+        if (SpriteBackend.isSpriteInClickableMode)
+        {
+            spriteRenderer.color = originalSpriteColor;
+        }
     }
 
     private void OnMouseDown()
     {
-        checkNeuronPart();
-        spriteContainer.transform.position = Vector2.Lerp(
-                spriteContainer.transform.position, 
-                spriteTextModePosition.transform.position, 
-                20.2f * Time.deltaTime
-            );
-
+        p.r(checkNeuronPart().ToString());
+        moveSpriteDown();
     }
 
-    public void checkNeuronPart()
+    public void moveSpriteDown()
+    {
+        if (SpriteBackend.isSpriteInClickableMode)
+        {
+            spriteContainer.transform.position = Vector2.Lerp(
+                            spriteContainer.transform.position,
+                            spriteTextModePosition.transform.position,
+                            20.2f * Time.deltaTime
+                        );
+            spriteRenderer.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+            SpriteBackend.isSpriteInClickableMode = false;
+        }
+        
+    }
+
+    public GameObject checkNeuronPart()
     {
         if (gameObject == neuron_akson)
         {
-            p.r("neuron_akson");
+            return neuron_akson;
         }
         else if (gameObject == neuron_cialo_komorki)
         {
-            p.r("neuron_cialo_komorki");
+            return neuron_cialo_komorki;
         }
         else if (gameObject == neuron_dendryty)
         {
-            p.r("neuron_dendryty");
+            return neuron_dendryty;
         }
         else if (gameObject == neuron_drzewko_koncowe)
         {
-            p.r("neuron_drzewko_koncowe");
+            return neuron_drzewko_koncowe;
         }
         else if (gameObject == neuron_jadro)
         {
-            p.r("neuron_jadro");
+            return neuron_jadro;
         }
         else if (gameObject == neuron_kolbki_synaptyczne)
         {
-            p.r("neuron_kolbki_synaptyczne");
+            return neuron_kolbki_synaptyczne;
         }
         else if (gameObject == neuron_odgalezienie_boczne)
         {
-            p.r("neuron_odgalezienie_boczne");
+            return neuron_odgalezienie_boczne;
         }
         else if (gameObject == neuron_odgalezienie_koncowe)
         {
-            p.r("neuron_odgalezienie_koncowe");
+            return neuron_odgalezienie_koncowe;
         }
         else if (gameObject == neuron_oslonka_mielinowa)
         {
-            p.r("neuron_oslonka_mielinowa");
+            return neuron_oslonka_mielinowa;
         }
         else if (gameObject == neuron_oslonka_schwanna)
         {
-            p.r("neuron_oslonka_schwanna");
+            return neuron_oslonka_schwanna;
         }
-        else if (gameObject == neuron_wezly_ranviera)
+        else
         {
-            p.r("neuron_wezly_ranviera");
+            return neuron_wezly_ranviera;
         }
     }
 }
