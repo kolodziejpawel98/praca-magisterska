1831f5f2c987e84f9d8df6bf084e575402753fb5
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-05-29 12:34:15 +0200

    refactor

--- praca_magisterska_gra_powazna/Assets/SampleObject/SpriteBackend.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/SpriteBackend.cs
@@ -5,5 +5,90 @@ using UnityEngine;
 public class SpriteBackend : MonoBehaviour
 {
     public static bool isSpriteInClickableMode = true;
+    public static bool isMoveDownAnimationTriggered = false;
+    public static GameObject selectedNeuronElement; //!!!!!!
+
+    public GameObject neuron_akson;
+    public GameObject neuron_cialo_komorki;
+    public GameObject neuron_dendryty;
+    public GameObject neuron_drzewko_koncowe;
+    public GameObject neuron_jadro;
+    public GameObject neuron_kolbki_synaptyczne;
+    public GameObject neuron_odgalezienie_boczne;
+    public GameObject neuron_odgalezienie_koncowe;
+    public GameObject neuron_oslonka_mielinowa;
+    public GameObject neuron_oslonka_schwanna;
+    public GameObject neuron_wezly_ranviera;
+
+
+    private void Update()
+    {
+        if (isMoveDownAnimationTriggered)
+        {
+            moveSpriteDown();
+        }
+    }
+
+    public void moveSpriteDown()
+    {
+        if (isSpriteInClickableMode)
+        {
+            gameObject.transform.position = Vector2.Lerp(
+                            gameObject.transform.position,
+                            new Vector3(1.13f, -7.58f, -0.64f),
+                            20.2f * Time.deltaTime
+                        );
+            isSpriteInClickableMode = false;
+        }
+
+    }
+
+    //public GameObject checkNeuronPart()
+    //{
+    //    if (selectedNeuronElement == neuron_akson)
+    //    {
+    //        return neuron_akson;
+    //    }
+    //    else if (selectedNeuronElement == neuron_cialo_komorki)
+    //    {
+    //        return neuron_cialo_komorki;
+    //    }
+    //    else if (selectedNeuronElement == neuron_dendryty)
+    //    {
+    //        return neuron_dendryty;
+    //    }
+    //    else if (selectedNeuronElement == neuron_drzewko_koncowe)
+    //    {
+    //        return neuron_drzewko_koncowe;
+    //    }
+    //    else if (selectedNeuronElement == neuron_jadro)
+    //    {
+    //        return neuron_jadro;
+    //    }
+    //    else if (selectedNeuronElement == neuron_kolbki_synaptyczne)
+    //    {
+    //        return neuron_kolbki_synaptyczne;
+    //    }
+    //    else if (selectedNeuronElement == neuron_odgalezienie_boczne)
+    //    {
+    //        return neuron_odgalezienie_boczne;
+    //    }
+    //    else if (selectedNeuronElement == neuron_odgalezienie_koncowe)
+    //    {
+    //        return neuron_odgalezienie_koncowe;
+    //    }
+    //    else if (selectedNeuronElement == neuron_oslonka_mielinowa)
+    //    {
+    //        return neuron_oslonka_mielinowa;
+    //    }
+    //    else if (selectedNeuronElement == neuron_oslonka_schwanna)
+    //    {
+    //        return neuron_oslonka_schwanna;
+    //    }
+    //    else if(selectedNeuronElement == neuron_wezly_ranviera)
+    //    {
+    //        return neuron_wezly_ranviera;
+    //    }
+    //}
 
 }
--- praca_magisterska_gra_powazna/Assets/SampleObject/SpriteHover.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/SpriteHover.cs
@@ -7,18 +7,6 @@ public class SpriteHover : MonoBehaviour
     public GameObject spriteContainer;
     public GameObject spriteTextModePosition;
 
-    public GameObject neuron_akson;
-    public GameObject neuron_cialo_komorki;
-    public GameObject neuron_dendryty;
-    public GameObject neuron_drzewko_koncowe;
-    public GameObject neuron_jadro;
-    public GameObject neuron_kolbki_synaptyczne;
-    public GameObject neuron_odgalezienie_boczne;
-    public GameObject neuron_odgalezienie_koncowe;
-    public GameObject neuron_oslonka_mielinowa;
-    public GameObject neuron_oslonka_schwanna;
-    public GameObject neuron_wezly_ranviera;
-
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -27,86 +15,24 @@ public class SpriteHover : MonoBehaviour
 
     private void OnMouseEnter()
     {
-        if (SpriteBackend.isSpriteInClickableMode)
-        {
+        //if (SpriteBackend.isSpriteInClickableMode)
+        //{
             spriteRenderer.color = new Color(5.0f, 0.0f, 0.0f, 0.9f);
-        }
+        //}
     }
 
     private void OnMouseExit()
     {
-        if (SpriteBackend.isSpriteInClickableMode)
-        {
+        //if (SpriteBackend.isSpriteInClickableMode)
+        //{
             spriteRenderer.color = originalSpriteColor;
-        }
+        //}
     }
 
     private void OnMouseDown()
     {
-        p.r(checkNeuronPart().ToString());
-        moveSpriteDown();
-    }
-
-    public void moveSpriteDown()
-    {
-        if (SpriteBackend.isSpriteInClickableMode)
-        {
-            spriteContainer.transform.position = Vector2.Lerp(
-                            spriteContainer.transform.position,
-                            spriteTextModePosition.transform.position,
-                            20.2f * Time.deltaTime
-                        );
-            spriteRenderer.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
-            SpriteBackend.isSpriteInClickableMode = false;
-        }
-        
-    }
-
-    public GameObject checkNeuronPart()
-    {
-        if (gameObject == neuron_akson)
-        {
-            return neuron_akson;
-        }
-        else if (gameObject == neuron_cialo_komorki)
-        {
-            return neuron_cialo_komorki;
-        }
-        else if (gameObject == neuron_dendryty)
-        {
-            return neuron_dendryty;
-        }
-        else if (gameObject == neuron_drzewko_koncowe)
-        {
-            return neuron_drzewko_koncowe;
-        }
-        else if (gameObject == neuron_jadro)
-        {
-            return neuron_jadro;
-        }
-        else if (gameObject == neuron_kolbki_synaptyczne)
-        {
-            return neuron_kolbki_synaptyczne;
-        }
-        else if (gameObject == neuron_odgalezienie_boczne)
-        {
-            return neuron_odgalezienie_boczne;
-        }
-        else if (gameObject == neuron_odgalezienie_koncowe)
-        {
-            return neuron_odgalezienie_koncowe;
-        }
-        else if (gameObject == neuron_oslonka_mielinowa)
-        {
-            return neuron_oslonka_mielinowa;
-        }
-        else if (gameObject == neuron_oslonka_schwanna)
-        {
-            return neuron_oslonka_schwanna;
-        }
-        else
-        {
-            return neuron_wezly_ranviera;
-        }
+        SpriteBackend.selectedNeuronElement = gameObject;
+        SpriteBackend.isMoveDownAnimationTriggered = true;
+        //moveSpriteDown();
     }
 }
