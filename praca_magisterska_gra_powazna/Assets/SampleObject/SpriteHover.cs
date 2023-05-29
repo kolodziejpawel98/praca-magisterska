e4de64a3cd12dcab0b20e717cc2abcb8058b5665
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-05-29 10:46:34 +0200

    wip

--- praca_magisterska_gra_powazna/Assets/SampleObject/SpriteHover.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/SpriteHover.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 
 public class SpriteHover : MonoBehaviour
 {
-    public GameObject neuron_akson;
-    public GameObject neuron_oslonka_mielinowa;
-    public GameObject neuron_oslonka_schwanna;
-
     private SpriteRenderer spriteRenderer;
     private Color originalSpriteColor;
 
@@ -17,18 +13,6 @@ public class SpriteHover : MonoBehaviour
 
     private void OnMouseEnter()
     {
-        if(gameObject == neuron_akson)
-        {
-            p.r("AKSON");
-        }
-        else if (gameObject == neuron_oslonka_mielinowa)
-        {
-            p.r("OSLONKA MIELINOWA");
-        }
-        else if (gameObject == neuron_oslonka_schwanna)
-        {
-            p.r("OSLONKA SCHWANNA");
-        }
         spriteRenderer.color = Color.red;
     }
 
