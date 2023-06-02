1828e0b185cf9e5c531e9ccffaabf2d1d78e43a2
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-06-02 22:05:34 +0200

    ranking bug fixed

--- praca_magisterska_gra_powazna/Assets/SampleObject/ScoreTable.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/ScoreTable.cs
@@ -14,42 +14,115 @@ public class ScoreTable : MonoBehaviour
 
     void Start()
     {
-        printPlayerScore();   
+        updateCurrentPlayerScore();
+        if(getCurrentPlayerIndex() > 9)
+        {
+            printTopTenPlayersWithoutCurrentPlayer();
+            printCurrentPlayerOnSide();
+        }
+        else
+        {
+            printTopTenPlayersWithCurrentPlayer();
+        }
     }
 
-    public void printPlayerScore()
+    public void printTopTenPlayersWithoutCurrentPlayer()
     {
         int scoreRowPositionCounter = 10;
         int topTenPlayersCounter = 0;
+
         foreach (var player in inputHandler.players)
         {
             GameObject playerNameText = Instantiate(prefabPlayerName, transform);
             GameObject playerNameScore = Instantiate(prefabPlayerScore, transform);
-            
-            if (player.isCurrentPlayer)
-            {
-                playerNameText.GetComponent<Text>().color = Color.yellow;
-                playerNameScore.GetComponent<Text>().color = Color.yellow;
-                player.points = TestBackend.score;
-                inputHandler.sortPlayers();
-                inputHandler.SaveToJSON(inputHandler.players);
-            }
-            
+
             playerNameText.transform.position = new Vector3(280, pointScoreTableFirstPlace.transform.position.y - scoreRowPositionCounter, 0);
             playerNameText.GetComponent<Text>().text = player.playerName;
             playerNameScore.transform.position = new Vector3(780, pointScoreTableFirstPlace.transform.position.y - scoreRowPositionCounter, 0);
             playerNameScore.GetComponent<Text>().text = player.points.ToString();
             scoreRowPositionCounter += 40;
             topTenPlayersCounter++;
+
             if (topTenPlayersCounter == 9)
             {
                 break;
             }
         }
-        inputHandler.sortPlayers();
-        //foreach (var player in inputHandler.players)
-        //{
-        //    p.r("player = " + player.playerName + " , score = " + player.points);    
-        //}
     }
+
+    public void printCurrentPlayerOnSide()
+    {
+        GameObject playerNameText = Instantiate(prefabPlayerName, transform);
+        GameObject playerNameScore = Instantiate(prefabPlayerScore, transform);
+
+        playerNameText.GetComponent<Text>().color = Color.yellow;
+        playerNameScore.GetComponent<Text>().color = Color.yellow;
+
+        playerNameText.transform.position = new Vector3(980, 400, 0);
+        playerNameText.GetComponent<Text>().text = inputHandler.players[getCurrentPlayerIndex()].playerName;
+        playerNameScore.transform.position = new Vector3(1280, 400, 0);
+        playerNameScore.GetComponent<Text>().text = inputHandler.players[getCurrentPlayerIndex()].points.ToString();
+    }
+
+    public void printTopTenPlayersWithCurrentPlayer()
+    {
+        int scoreRowPositionCounter = 10;
+        int topTenPlayersCounter = 0;
+
+        foreach (var player in inputHandler.players)
+        {
+            GameObject playerNameText = Instantiate(prefabPlayerName, transform);
+            GameObject playerNameScore = Instantiate(prefabPlayerScore, transform);
+
+
+            if (player.isCurrentPlayer)
+            {
+                playerNameText.GetComponent<Text>().color = Color.yellow;
+                playerNameScore.GetComponent<Text>().color = Color.yellow;
+            }
+
+            playerNameText.transform.position = new Vector3(280, pointScoreTableFirstPlace.transform.position.y - scoreRowPositionCounter, 0);
+            playerNameText.GetComponent<Text>().text = player.playerName;
+            playerNameScore.transform.position = new Vector3(780, pointScoreTableFirstPlace.transform.position.y - scoreRowPositionCounter, 0);
+            playerNameScore.GetComponent<Text>().text = player.points.ToString();
+            scoreRowPositionCounter += 40;
+            topTenPlayersCounter++;
+
+            if (topTenPlayersCounter == 9)
+            {
+                break;
+            }
+        }
+    }
+
+    public int getCurrentPlayerIndex()
+    {
+        int index = 0;
+        foreach (var player in inputHandler.players)
+        {
+            if (player.isCurrentPlayer)
+            {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+
+    public void updateCurrentPlayerScore()
+    {
+        //update'uje legitnym wynikiem pole points gracza, ktore przy tworzeniu zostal wypelniny losowa wartoscia
+        foreach (var player in inputHandler.players)
+        {
+            if (player.isCurrentPlayer)
+            {
+                player.points = TestBackend.score;
+                inputHandler.sortPlayers();
+                inputHandler.SaveToJSON(inputHandler.players);
+            }
+        }
+    }
+
+
+
 }
