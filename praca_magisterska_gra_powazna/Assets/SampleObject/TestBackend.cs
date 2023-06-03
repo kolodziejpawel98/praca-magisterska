8168546220db9758c9dfae49223d49dd76b99ea0
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-06-03 17:56:01 +0200

    wip

--- praca_magisterska_gra_powazna/Assets/SampleObject/TestBackend.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/TestBackend.cs
@@ -1,224 +1,3 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-
-//public class Answer
-//{
-//    public Button answerButton;
-//    public string answerText;
-//    public bool isAnswerCorrect;
-//    public bool isButtonActive;
-
-//    List<Answer> answers = new List<Answer>();
-
-//    public Answer(){}
-
-//    public Answer(Button answerButton, string answerText, bool isAnswerCorrect, bool isButtonActive)
-//    {
-//        this.answerText = answerText;
-//        this.answerButton = answerButton;
-//        this.isAnswerCorrect = isAnswerCorrect;
-//        this.isButtonActive = isButtonActive;
-//    }
-
-//    public void addAnswer(Button answerButton, string answerText, bool isAnswerCorrect, bool isButtonActive = true)
-//    {
-//        answers.Add(new Answer(answerButton, answerText, isAnswerCorrect, isButtonActive));
-//    }
-
-//    public void answerCorectnessHandler(List<Answer> answers)
-//    {
-//        if (isAnswerCorrect)
-//        {
-//            answerButton.onClick.AddListener(() => correctAnswerHandler(answers));
-
-//        }
-//        else
-//        {
-//            answerButton.onClick.AddListener(wrongAnswerHandler);
-//        }
-//    }
-
-//    public void removeButtonListener()
-//    {
-//        answerButton.onClick.RemoveAllListeners();
-//    }
-
-//    public void correctAnswerHandler(List<Answer> answers)
-//    {
-//        Timer.StopTimer();
-//        TestBackend.timeScore = Timer.GetElapsedTimeInSeconds();
-//        TestBackend.score += Timer.calculateTimeToPoints();
-//        answerButton.image.color = Color.green;
-//        foreach (var answer in answers)
-//        {
-//            if (!answer.isAnswerCorrect)
-//            {
-//                answer.answerButton.interactable = false;
-//            }
-//        }
-//    }
-
-//    public  void wrongAnswerHandler()
-//    {
-//        answerButton.image.color = Color.red;
-//    }
-
-//    public void resetEverything()
-//    {
-//        removeButtonListener();
-//        answerButton.image.color = Color.white;
-//    }
-//}
-
-//public class TestBackend : MonoBehaviour
-//{
-//    public Text scoreText;
-//    public static float score = 0.0f;
-//    public Text timeScoreText;
-//    public static float timeScore = 0.0f;
-
-//    public int currentQuestionNumber = 1;
-//    public bool enterOnceInUpdate = false;
-
-//    public Text question;
-//    public Button answer_A_button;
-//    public Button answer_B_button;
-//    public Button answer_C_button;
-//    public Button answer_D_button;
-//    //List<Answer> answers = new List<Answer>();
-//    Answer answers;
-
-//    public GameObject buttonGoToResume;
-//    public GameObject buttonNextQuestion;
-
-//    void Start()
-//    {
-//        //goToResumeButton.interactable = false;
-//        buttonGoToResume.SetActive(false);
-//        printTextElementOnScreen(question);
-//        printTextElementOnScreen(answer_A_button);
-//        printTextElementOnScreen(answer_B_button);
-//        printTextElementOnScreen(answer_C_button);
-//        printTextElementOnScreen(answer_D_button); 
-//    }
-
-//    private void Update()
-//    {
-//        scoreText.text = score.ToString();
-//        timeScoreText.text = timeScore.ToString();
-//        switch (currentQuestionNumber)
-//        {
-//            case 1:
-//                if (!enterOnceInUpdate)
-//                {
-//                    Timer.StartTimer();
-//                    updateTextElementOnScreen(question, "Który p³at mózgowy nie istnieje?");
-
-//                    answers.addAnswer(answer_A_button, "czo³owy", false);
-//                    answers.addAnswer(answer_B_button, "polityczny", true);
-//                    answers.addAnswer(answer_C_button, "skroniowy", false);
-//                    answers.addAnswer(answer_D_button, "ciemieniowy", false);
-//                    updateTextElementOnScreen(answers);
-
-//                    enterOnceInUpdate = true;
-//                }
-//                break;
-//            case 2:
-//                if (!enterOnceInUpdate)
-//                {
-//                    Timer.StartTimer();
-//                    updateTextElementOnScreen(question, "Lewa pó³kula odpowiada za:");
-
-//                    answers.Add(new Answer(answer_A_button, "logikê", true));
-//                    answers.Add(new Answer(answer_B_button, "kreatywnoœæ", false));
-//                    answers.Add(new Answer(answer_C_button, "-", false, false));
-//                    answers.Add(new Answer(answer_D_button, "-", false, false));
-//                    updateTextElementOnScreen(answers);
-
-//                    enterOnceInUpdate = true;
-//                }
-//                break;
-//            case 3:
-//                if (!enterOnceInUpdate)
-//                {
-//                    Timer.StartTimer();
-//                    updateTextElementOnScreen(question, "Osoba uderzy³a siê mocno w ty³ g³owy, i uszkodzi³a (?) p³at potyliczny. Jaki zmys³ móg³ ucierpieæ?", 20);
-
-//                    answers.Add(new Answer(answer_A_button, "wzrok", true));
-//                    answers.Add(new Answer(answer_B_button, "s³uch", false));
-//                    answers.Add(new Answer(answer_C_button, "mowa", false));
-//                    answers.Add(new Answer(answer_D_button, "wêch", false));
-//                    updateTextElementOnScreen(answers);
-
-//                    enterOnceInUpdate = true;
-//                    //goToResumeButton.interactable = true;
-//                    buttonGoToResume.SetActive(true);
-//                    buttonNextQuestion.SetActive(false);
-//                }
-//                break;
-//        }
-//    }
-
-//    public void printTextElementOnScreen(Button answer, string text = "", int x = -426, int y = 39, int fontSize = 47)
-//    {
-//        Text textPrefab = answer.GetComponentInChildren<Text>();
-//        Text newText = Instantiate(textPrefab, transform);
-//        newText.text = text;
-//        newText.color = Color.white;
-//        newText.fontSize = fontSize;
-//        RectTransform rectTransform = newText.GetComponent<RectTransform>();
-//        rectTransform.anchoredPosition = new Vector2(x, y);
-//    }
-
-//    public void printTextElementOnScreen(Text answer, string text = "", int x = -426, int y = 39, int fontSize = 47)
-//    {
-//        Text newText = Instantiate(answer, transform);
-//        newText.text = text;
-//        newText.color = Color.white;
-//        newText.fontSize = fontSize;
-//        RectTransform rectTransform = newText.GetComponent<RectTransform>();
-//        rectTransform.anchoredPosition = new Vector2(x, y);
-//    }
-
-//    public void updateTextElementOnScreen(List<Answer> answers)
-//    {
-//        foreach (var answer in answers)
-//        {
-//            if (answer.isButtonActive)
-//            {
-//                answer.answerButton.interactable = true;
-//            }
-//            else
-//            {
-//                answer.answerButton.interactable = false;
-//            }
-//            answer.answerCorectnessHandler(answers);
-//            answer.answerButton.GetComponentInChildren<Text>().text = answer.answerText;
-//        }
-//    }
-
-//    public void updateTextElementOnScreen(Text answer, string text, int fontSize = 47, bool isButtonActive = true)
-//    {
-//        answer.GetComponentInChildren<Text>().text = text;
-//        answer.GetComponentInChildren<Text>().fontSize = fontSize;
-//    }
-
-//    public void nextQuestion()
-//    {
-//        currentQuestionNumber++;
-//        enterOnceInUpdate = false;
-
-//        foreach (var answer in answers)
-//        {
-//            answer.resetEverything();
-//        }
-//        answers.RemoveRange(0, 4);
-//    }
-
-//}
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -316,27 +95,26 @@ public class TestBackend : MonoBehaviour
                     answers.Add(new Answer(answer_B_button, "s³uch", false));
                     answers.Add(new Answer(answer_C_button, "mowa", false));
                     answers.Add(new Answer(answer_D_button, "wêch", false));
-                    updateTextElementOnScreen();
+                    updateTextElementOnScreen(true);
 
                     enterOnceInUpdate = true;
                     //goToResumeButton.interactable = true;
-                    buttonGoToResume.SetActive(true);
                     buttonNextQuestion.SetActive(false);
                 }
                 break;
         }
     }
 
-    public void answerCorrectnessHandler(Answer answer)
+    public void answerCorrectnessHandler(Answer answer, bool isLastQuestion)
     {
         if (answer.isAnswerCorrect)
         {
-            answer.answerButton.onClick.AddListener(() => correctAnswerHandler(answer));
+            answer.answerButton.onClick.AddListener(() => correctAnswerHandler(answer, isLastQuestion));
 
         }
         else
         {
-            answer.answerButton.onClick.AddListener(() => wrongAnswerHandler(answer));
+            answer.answerButton.onClick.AddListener(() => wrongAnswerHandler(answer, isLastQuestion));
         }
     }
 
@@ -345,7 +123,7 @@ public class TestBackend : MonoBehaviour
         answer.answerButton.onClick.RemoveAllListeners();
     }
 
-    public void correctAnswerHandler(Answer answer)
+    public void correctAnswerHandler(Answer answer, bool isLastQuestion)
     {
         Timer.StopTimer();
         timeScore = Timer.GetElapsedTimeInSeconds();
@@ -359,11 +137,21 @@ public class TestBackend : MonoBehaviour
                 answer2.answerButton.interactable = false;
             }
         }
+
+        if (isLastQuestion)
+        {
+            buttonGoToResume.SetActive(true);
+        }
     }
 
-    public void wrongAnswerHandler(Answer answer)
+    public void wrongAnswerHandler(Answer answer, bool isLastQuestion)
     {
         answer.answerButton.image.color = Color.red;
+        
+        if (isLastQuestion)
+        {
+            buttonGoToResume.SetActive(true);
+        }
     }
 
     public void resetEverything(Answer answer)
@@ -393,7 +181,7 @@ public class TestBackend : MonoBehaviour
         rectTransform.anchoredPosition = new Vector2(x, y);
     }
 
-    public void updateTextElementOnScreen()
+    public void updateTextElementOnScreen(bool isLastQuestion = false)
     {
         foreach (var answer in answers)
         {
@@ -405,7 +193,7 @@ public class TestBackend : MonoBehaviour
             {
                 answer.answerButton.interactable = false;
             }
-            answerCorrectnessHandler(answer);
+            answerCorrectnessHandler(answer, isLastQuestion);
             answer.answerButton.GetComponentInChildren<Text>().text = answer.answerText;
         }
     }
