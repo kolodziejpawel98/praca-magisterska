e3c7a95d5ccbbd17c44eba984d49b1399a52f950
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-06-03 11:47:39 +0200

    neuron refactor base

--- praca_magisterska_gra_powazna/Assets/SampleObject/SpriteBackend.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/SpriteBackend.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SpriteBackend : MonoBehaviour
 {
@@ -8,10 +9,21 @@ public class SpriteBackend : MonoBehaviour
     public static GameObject selectedNeuronElement;
     public static bool isgoBackToViewModeButtonActive = false;
     public static bool isBackToViewModeAnimationTriggered = false;
-    
-    public void goBackToViewSpriteMode()
+
+    public GameObject jadro_neuronu;
+
+    public event Action<GameObject> OnChildClicked;
+
+    public void ChildEntered(GameObject child)
     {
-        isgoBackToViewModeButtonActive = false;
-        isBackToViewModeAnimationTriggered = true;
+        // Wywo³anie zdarzenia OnChildClicked
+        OnChildClicked?.Invoke(child);
+
+
+        if (child == jadro_neuronu)
+        {
+            p.r("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! JADRO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        }
+
     }
 }
--- praca_magisterska_gra_powazna/Assets/SampleObject/SpriteHover.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/SpriteHover.cs
@@ -11,114 +11,145 @@ public class SpriteHover : MonoBehaviour
     private bool isSpriteBackToViewMode = false;
     public GameObject neuronElementTextContainer;
 
+    private SpriteBackend spriteBackend;
+
     private void Start()
     {
-        goBackToViewModeButton.gameObject.SetActive(false);
-
-        originalSpriteElementColor = new Color(
-                GetComponent<SpriteRenderer>().color.r,
-                GetComponent<SpriteRenderer>().color.g,
-                GetComponent<SpriteRenderer>().color.b, 
-                1.0f
-            );
-    }
-
-    private void Update()
-    {
-        if (SpriteBackend.isMoveDownAnimationTriggered && !spriteElementSelected)
-        {
-            setTextModeColor(); //ustawienie wszystkim (poza kliknietym) elementom ciemnego koloru
-        }
-
-        if (SpriteBackend.isgoBackToViewModeButtonActive)
-        {
-            goBackToViewModeButton.gameObject.SetActive(true);
-            isSpriteBackToViewMode = true;
-        }
-        else
-        {
-            goBackToViewModeButton.gameObject.SetActive(false);
-            neuronElementTextContainer.SetActive(false);
-            if (isSpriteBackToViewMode) //dzieki temu flaga ustawi siê gdy sprite jest na dole, a to uruchomi za gdy sprite worci na gore
-            {
-                setDefaultColor();
-                isSpriteBackToViewMode = false;
-                spriteElementSelected = false;
-            }
-        }
-
-        if (SpriteBackend.isBackToViewModeAnimationTriggered)
-        {
-            moveSpriteToViewPosition();
-            SpriteBackend.isBackToViewModeAnimationTriggered = false;
-            SpriteBackend.isMoveDownAnimationTriggered = false;  
-        }
-    }
-
-    private void OnMouseEnter()
-    {
-        if (!SpriteBackend.isMoveDownAnimationTriggered)
-        {
-            setTriggeredElementColor();
-        }
-    }
-
-    private void OnMouseExit()
-    {
-        if (!SpriteBackend.isMoveDownAnimationTriggered)
-        {
-            setDefaultColor();
-        }
+        // Przechwycenie referencji do obiektu SpriteBackend
+        spriteBackend = GetComponentInParent<SpriteBackend>();
     }
 
     private void OnMouseDown()
     {
-        if (!SpriteBackend.isMoveDownAnimationTriggered)
-        {
-            SpriteBackend.selectedNeuronElement = gameObject;
-            SpriteBackend.isMoveDownAnimationTriggered = true;
-            moveSpriteToTextPosition();
-            setDefaultColor(); //dzieki temu klikniety element sie nie bedzie swiecil, a w update() zasiweca sie wszystkie inne
-            spriteElementSelected = true;
-            SpriteBackend.isgoBackToViewModeButtonActive = true;
-            neuronElementTextContainer.SetActive(true);
-        }
+        // Wywo³anie metody ChildEntered w obiekcie SpriteBackend
+        spriteBackend.ChildEntered(gameObject);
     }
 
-    public void moveSpriteToTextPosition()
-    {
-        spriteContainer.transform.position = Vector2.Lerp(
-                        spriteContainer.transform.position,
-                        spriteTextModePosition.transform.position,
-                        100.2f * Time.deltaTime
-                    );
 
-    }
 
-    public void moveSpriteToViewPosition()
-    {
-        spriteContainer.transform.position = Vector2.Lerp(
-                        spriteContainer.transform.position,
-                        spriteViewModePosition.transform.position,
-                        100.2f * Time.deltaTime
-                    );
 
-    }
 
-    public void setTriggeredElementColor()
-    {
-        GetComponent<SpriteRenderer>().color = new Color(5.0f, 0.0f, 0.0f, 0.9f);
-    }
 
-    public void setTextModeColor()
-    {
-        Color helpColorSave = originalSpriteElementColor;
-        helpColorSave.a = 0.2f;
-        GetComponent<SpriteRenderer>().color = helpColorSave;
-    }
 
-    public void setDefaultColor()
-    {
-        GetComponent<SpriteRenderer>().color = originalSpriteElementColor;
-    }
+
+
+
+
+
+
+
+
+
+
+
+    //private void Start()
+    //{
+    //    goBackToViewModeButton.gameObject.SetActive(false);
+
+    //    originalSpriteElementColor = new Color(
+    //            GetComponent<SpriteRenderer>().color.r,
+    //            GetComponent<SpriteRenderer>().color.g,
+    //            GetComponent<SpriteRenderer>().color.b, 
+    //            1.0f
+    //        );
+    //}
+
+    //private void Update()
+    //{
+    //    if (SpriteBackend.isMoveDownAnimationTriggered && !spriteElementSelected)
+    //    {
+    //        setTextModeColor(); //ustawienie wszystkim (poza kliknietym) elementom ciemnego koloru
+    //    }
+
+    //    if (SpriteBackend.isgoBackToViewModeButtonActive)
+    //    {
+    //        goBackToViewModeButton.gameObject.SetActive(true);
+    //        isSpriteBackToViewMode = true;
+    //    }
+    //    else
+    //    {
+    //        goBackToViewModeButton.gameObject.SetActive(false);
+    //        neuronElementTextContainer.SetActive(false);
+    //        if (isSpriteBackToViewMode) //dzieki temu flaga ustawi siê gdy sprite jest na dole, a to uruchomi za gdy sprite worci na gore
+    //        {
+    //            setDefaultColor();
+    //            isSpriteBackToViewMode = false;
+    //            spriteElementSelected = false;
+    //        }
+    //    }
+
+    //    if (SpriteBackend.isBackToViewModeAnimationTriggered)
+    //    {
+    //        moveSpriteToViewPosition();
+    //        SpriteBackend.isBackToViewModeAnimationTriggered = false;
+    //        SpriteBackend.isMoveDownAnimationTriggered = false;  
+    //    }
+    //}
+
+    //private void OnMouseEnter()
+    //{
+    //    if (!SpriteBackend.isMoveDownAnimationTriggered)
+    //    {
+    //        setTriggeredElementColor();
+    //    }
+    //}
+
+    //private void OnMouseExit()
+    //{
+    //    if (!SpriteBackend.isMoveDownAnimationTriggered)
+    //    {
+    //        setDefaultColor();
+    //    }
+    //}
+
+    //private void OnMouseDown()
+    //{
+    //    if (!SpriteBackend.isMoveDownAnimationTriggered)
+    //    {
+    //        SpriteBackend.selectedNeuronElement = gameObject;
+    //        SpriteBackend.isMoveDownAnimationTriggered = true;
+    //        moveSpriteToTextPosition();
+    //        setDefaultColor(); //dzieki temu klikniety element sie nie bedzie swiecil, a w update() zasiweca sie wszystkie inne
+    //        spriteElementSelected = true;
+    //        SpriteBackend.isgoBackToViewModeButtonActive = true;
+    //        neuronElementTextContainer.SetActive(true);
+    //    }
+    //}
+
+    //public void moveSpriteToTextPosition()
+    //{
+    //    spriteContainer.transform.position = Vector2.Lerp(
+    //                    spriteContainer.transform.position,
+    //                    spriteTextModePosition.transform.position,
+    //                    100.2f * Time.deltaTime
+    //                );
+
+    //}
+
+    //public void moveSpriteToViewPosition()
+    //{
+    //    spriteContainer.transform.position = Vector2.Lerp(
+    //                    spriteContainer.transform.position,
+    //                    spriteViewModePosition.transform.position,
+    //                    100.2f * Time.deltaTime
+    //                );
+
+    //}
+
+    //public void setTriggeredElementColor()
+    //{
+    //    GetComponent<SpriteRenderer>().color = new Color(5.0f, 0.0f, 0.0f, 0.9f);
+    //}
+
+    //public void setTextModeColor()
+    //{
+    //    Color helpColorSave = originalSpriteElementColor;
+    //    helpColorSave.a = 0.2f;
+    //    GetComponent<SpriteRenderer>().color = helpColorSave;
+    //}
+
+    //public void setDefaultColor()
+    //{
+    //    GetComponent<SpriteRenderer>().color = originalSpriteElementColor;
+    //}
 }
