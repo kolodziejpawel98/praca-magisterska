c6ad97a1b43d79f7cedc609cf306e894987fab76
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-06-08 17:06:22 +0200

    tutorial done

--- praca_magisterska_gra_powazna/Assets/SampleObject/TestTutorial.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/TestTutorial.cs
@@ -6,10 +6,23 @@ public class TestTutorial : MonoBehaviour
 {
     public List<GameObject> tutorialSteps = new List<GameObject>();
     private int currentStepIndex = 0;
+    public GameObject buttonNextStep;
+    public GameObject buttonGoToTest;
+
+    private void Start()
+    {
+        buttonGoToTest.SetActive(false);
+    }
 
     public void Update()
     {
         tutorialSteps[currentStepIndex].SetActive(true);
+
+        if(currentStepIndex == 7)
+        {
+            buttonGoToTest.SetActive(true);
+            buttonNextStep.SetActive(false);
+        }
     }
 
     public void nextTutorialStep()
