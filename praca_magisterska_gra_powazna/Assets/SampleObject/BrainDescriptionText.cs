237286bf5a4212a126e4fafb27d2820e96cb91c5
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-06-14 21:14:10 +0200

    wip

--- praca_magisterska_gra_powazna/Assets/SampleObject/BrainDescriptionText.cs
+++ praca_magisterska_gra_powazna/Assets/SampleObject/BrainDescriptionText.cs
@@ -5,4 +5,52 @@ using UnityEngine;
 public class BrainDescriptionText : MonoBehaviour
 {
     public BrainDescriptionText textContainer;
+
+    public List<GameObject> slideImages = new List<GameObject>();
+    public List<GameObject> slideTexts = new List<GameObject> ();
+
+    public int currentSlideIndex = 0;
+
+    private void Start()
+    {
+        foreach (GameObject image in slideImages)
+        {
+            image.SetActive(false);
+        }
+        
+        foreach (GameObject text in slideTexts)
+        {
+            text.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if(slideImages.Count > 0)
+        {
+            slideImages[currentSlideIndex].SetActive(true);
+            slideTexts[currentSlideIndex].SetActive(true);
+        }
+    }
+
+    public void nextSlide()
+    {
+        if(currentSlideIndex < slideImages.Count - 1)
+        {
+            slideImages[currentSlideIndex].SetActive(false);
+            slideTexts[currentSlideIndex].SetActive(false);
+            currentSlideIndex++;
+        }
+    }
+
+    public void previousSlide()
+    {
+        if(currentSlideIndex > 0)
+        {
+            slideImages[currentSlideIndex].SetActive(false);
+            slideTexts[currentSlideIndex].SetActive(false);
+            currentSlideIndex--;
+        }
+    }
+
 }
